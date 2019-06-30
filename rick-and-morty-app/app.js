36886a78df39ffe0ba1c716e04761c4c9e983acb
Quincy Hicks <qdh6159@gmail.com>
2019-06-30 12:27:54 -0500

    fixed duplicate favorites click issue

--- rick-and-morty-app/app.js
+++ rick-and-morty-app/app.js
@@ -1,194 +1,83 @@
-// console.log("Wubba lubba dub-dub!")
-
-// function loopCharacters(characterArray) {
-//     for(let i = `${start}`; i < characterArray.length; i++) {
-//         let characterId = characterArray[i].id
-//         let characterName = characterArray[i].name
-//         let characterStatus = characterArray[i].status
-//         let characterSpecies = characterArray[i].species
-//         let characterType = characterArray[i].type
-//         let characterGender = characterArray[i].gender
-//         let characterLocation = characterArray[i].location.name
-//         let characterOrigin = characterArray[i].origin.name
-//         // console.log(characterId, characterName, characterSpecies, characterType, characterGender, characterLocation, characterOrigin)
-//         // console.log(characterStatus)
-//         // console.log(characterId)
-//         let name = characterArray[i].name
-
-//         // variables for each character detail window below
-//         let image = `<img class="divImage" src="${characterArray[i].image}"></img>`
-//         let spanName = `<span>Name: ${characterName}</span>`
-//         let spanId = `<span>Character id: ${characterId}</span>`
-//         let spanStatus = `<span>Status: ${characterStatus}</span>`
-//         let spanSpecies = `<span>Species: ${characterSpecies}</span>`
-//         let spanType = `<span>Type: ${characterType}</span>`
-//         let spanGender = `<span>Gender: ${characterGender}</span>`
-//         let spanLocation = `<span>Location: ${characterLocation}</span>`
-//         let spanOrigin = `<span>Origin: ${characterOrigin}</span>`
-
-//         const divWindow = `<div class="detailDiv">${spanName}<br><br>${spanId}<br>${spanStatus}<br>${spanSpecies}<br>${spanType}<br>${spanGender}<br>${spanLocation}<br>${spanOrigin}</div>`
-
-//         let pElementName = $('#mainBox').append(`<li class="fighter-list__item"><div class="transitionSetup"><div class="listDiv">${image}${divWindow}</div></div></div></li>`);
-//         pElementName;
-//         // const detailWindow = $('.detailDiv')
-//         // detailWindow.append(`<h4>${characterName}</h4>`)
-//         $('.detailDiv').hide()
-//     };
-// };
-
-// let start = 0
-// let ricksArray = []; 
-// let squanchysFavoriteSquanch = []
-// let characterPageNumber = 1
-// let interdimensionalTrafficLight = 'green'
-// let urlC137 = "https://rickandmortyapi.com/api/character/?page=4"
-
-// let fetchCharacters = {
-//     // url:`https://rickandmortyapi.com/api/character/?page=${characterPageNumber}`,
-//     url: urlC137,
-//     success: (data)=>{
-//         console.log(data);
-//         // characterArray = data.results
-//         for(let i = 0; i < data.results.length; i++){
-//             console.log(data.results[i])
-//             console.log(data.info.next)
-//             urlC137 = data.info.next
-//             ricksArray.push(data.results[i])
-//             console.log(`Pushed ${data.results[i].name} to the character array`)
-//         }
-//         loopCharacters(ricksArray)
-        
-
-
-
-
-//         $(".divImage").on({
-//             mouseenter: function () {
-//                 $(".detailDiv").addClass('changeDetailDiv');
-//                 $(this).next().fadeIn()
-//             },
-//             mouseleave: function () {
-//                 $(".detailDiv").removeClass('changeDetailDiv');
-//                 $(this).next().fadeOut()
-//             }
-//         });
-//     },
-//     error: ()=>{
-//         console.log('bad request');
-//     }
-// }
-// $.ajax(fetchCharacters)
-
-
-// // https://www.youtube.com/watch?v=76IANst0jwc&list=LLP-c0WGDa1fFuSs3QUYC4dQ&index=3&t=0s
-// $(window).scroll(function(){
-//     if($(this).scrollTop() + 1 >= $('body').height() - $(window).height())
-//         if(interdimensionalTrafficLight = 'green') {
-//             interdimensionalTrafficLight = 'red'
-//             console.log('new page would load now')
-//             // characterPageNumber ++
-//             start += 20
-//             console.log(characterPageNumber)
-//             $.ajax(fetchCharacters)
-
-//             setTimeout(function(){
-//                 interdimensionalTrafficLight = 'green'
-//             }, 2000)
-//         }
-// });
-
-// https://www.youtube.com/watch?v=chavThlNz3s
-
-
-
-
-
-
-function loopCharacters(characterArray) {
-    for(let i = 0; i < characterArray.length; i++) {
-        let characterId = characterArray[i].id
-        let characterName = characterArray[i].name
-        let characterStatus = characterArray[i].status
-        let characterSpecies = characterArray[i].species
-        let characterType = characterArray[i].type
-        let characterGender = characterArray[i].gender
-        let characterLocation = characterArray[i].location.name
-        let characterOrigin = characterArray[i].origin.name
-        // console.log(characterId, characterName, characterSpecies, characterType, characterGender, characterLocation, characterOrigin)
-        // console.log(characterStatus)
-        // console.log(characterId)
-        let name = characterArray[i].name
-
-        // variables for each character detail window below
-        let image = `<img class="divImage" src="${characterArray[i].image}"></img>`
-        let spanName = `<span>Name: ${characterName}</span>`
-        let spanId = `<span>Character id: ${characterId}</span>`
-        let spanStatus = `<span>Status: ${characterStatus}</span>`
-        let spanSpecies = `<span>Species: ${characterSpecies}</span>`
-        let spanType = `<span>Type: ${characterType}</span>`
-        let spanGender = `<span>Gender: ${characterGender}</span>`
-        let spanLocation = `<span>Location: ${characterLocation}</span>`
-        let spanOrigin = `<span>Origin: ${characterOrigin}</span>`
-
-        const divWindow = `<div class="detailDiv">${spanName}<br><br>${spanId}<br>${spanStatus}<br>${spanSpecies}<br>${spanType}<br>${spanGender}<br>${spanLocation}<br>${spanOrigin}</div>`
-
-        let pElementName = $('#mainBox').append(`<li class="fighter-list__item"><div class="transitionSetup"><div class="listDiv">${image}${divWindow}</div></div></div></li>`);
-        pElementName;
-
-        $(".divImage").on({
-            mouseenter: function () {
-                $(".detailDiv").addClass('changeDetailDiv');
-                $(this).next().fadeIn()
-            },
-            mouseleave: function () {
-                $(".detailDiv").removeClass('changeDetailDiv');
-                $(this).next().fadeOut()
-            }
-        });
-
-        // const detailWindow = $('.detailDiv')
-        // detailWindow.append(`<h4>${characterName}</h4>`)
-        $('.detailDiv').hide()
-    };
-};
+console.log("Wubba lubba dub-dub!")
 
 let ricksArray = []; 
-// let squanchysFavoriteSquanch = []
+let squanchysFavoriteSquanch = []
 // let characterPageNumber = 1
 let interdimensionalTrafficLight = 'green'
 let urlC137 
-
-
 let rickapedia = "https://rickandmortyapi.com/api/character/?page=1"
 
-function setup() {
-    portalGun(rickapedia)
-        .then(data =>{
-            console.log(data)
-            console.log(data.info.next)
-            // Pull data from API
-            ricksArray = data.results
-            rickapedia = data.info.next
-            loopCharacters(ricksArray)
+    async function portalGun(url) {
+        let response = await fetch(url)
+        let data = await response.json()
+        return data
+        }
+    
 
+        
+
+        function setup() {
+            portalGun(rickapedia)
+                .then(data =>{
+                    console.log(data)
+                    console.log(data.info.next)
+                    // Pull data from API
+                    ricksArray = data.results
+                    rickapedia = data.info.next
+                    loopCharacters(ricksArray)
+                    $(".divImage").on({
+                        mouseenter: function () {
+                            $(".detailDiv").addClass('changeDetailDiv');
+                            $(this).next().fadeIn()
+                            // $('.detailDiv').fadeIn()
+                            
+                        },
+                        mouseleave: function () {
+                            $(".detailDiv").removeClass('changeDetailDiv')
+                            $(this).next().fadeOut()
+                            // $('.detailDiv').fadeOut()
+                            
+                        }
+                    });
             
+                    $(".listDiv").on({
+                        mouseenter: function () {
+                            $(this).find("i").addClass('changeOfHeart');
+                            
+                        },
+                        mouseleave: function () {
+                            $(this).find("i").removeClass('changeOfHeart')
+                        
+                            
+                        }
+                    });
             
-        })
-        .catch(err => console.error(err))
-}
+                    
+                    $(".listDiv").closest("i").on("click", function(){
+                        console.log("heart clicked ")
+                        $(event.target).toggleClass("fas")
+                    })
+            
+                    $(".divImage").on("click", function(){
+                        console.log($(event.target).attr("alt"))
+                        let favCharacter = $(event.target).attr("alt")
+                        alert("added this character to your favorites")
+                        squanchysFavoriteSquanch.push(favCharacter)
+            
+                    })
+            
+                    
+                    $('.detailDiv').hide()
+                    
 
+                    
+                    
+                })
+                .catch(err => console.error(err))
+        }
+        setup();
 
-
-
-async function portalGun(url) {
-let response = await fetch(url)
-let data = await response.json()
-return data
-}
-
-setup();
-
-
+// Infinite Scroll
 // https://www.youtube.com/watch?v=76IANst0jwc&list=LLP-c0WGDa1fFuSs3QUYC4dQ&index=3&t=0s
 $(window).scroll(function(){
     if($(this).scrollTop() + 1 >= $('body').height() - $(window).height())
@@ -205,14 +94,90 @@ $(window).scroll(function(){
         }
 });
 
+function loopCharacters(characterArray) {
+    for(let i = 0; i < characterArray.length; i++) {
+        let characterId = characterArray[i].id
+        let characterName = characterArray[i].name
+        let characterStatus = characterArray[i].status
+        let characterSpecies = characterArray[i].species
+        let characterType = characterArray[i].type
+        let characterGender = characterArray[i].gender
+        let characterLocation = characterArray[i].location.name
+        let characterOrigin = characterArray[i].origin.name
+        let characterUrl = characterArray[i].url
+        // console.log(characterId, characterName, characterSpecies, characterType, characterGender, characterLocation, characterOrigin)
+        // console.log(characterStatus)
+        // console.log(characterId)
+        let name = characterArray[i].name                       //<div id="ghost"></div>
+
+        // variables for each character detail window below
+        
+        let spanName = `<span>Name: ${characterName}</span>`
+        let spanId = `<span>Character id: ${characterId}</span>`
+        let spanStatus = `<span>Status: ${characterStatus}</span>`
+        let spanSpecies = `<span>Species: ${characterSpecies}</span>`
+        let spanType = `<span>Type: ${characterType}</span>`
+        let spanGender = `<span>Gender: ${characterGender}</span>`
+        let spanLocation = `<span>Location: ${characterLocation}</span>`
+        let spanOrigin = `<span>Origin: ${characterOrigin}</span>`
+        // let spanUrl = `<span id="hide">${characterUrl}</span>`
+
+        let image = `<i class="far fa-heart"></i><img class="divImage" src="${characterArray[i].image}" alt=${characterUrl}>`
+
+        const divWindow = `<div class="detailDiv">${spanName}<br><br>${spanId}<br>${spanStatus}<br>${spanSpecies}<br>${spanType}<br>${spanGender}<br>${spanLocation}<br>${spanOrigin}</div>`
+
+        let pElementName = $('#mainBox').append(`<li class="fighter-list__item"><div class="transitionSetup"><div class="listDiv">${image}${divWindow}</div></div></div></li>`);
+        pElementName;
+
+        
+
+        // $(".divImage").on({
+        //     mouseenter: function () {
+        //         $(".detailDiv").addClass('changeDetailDiv');
+        //         $(this).next().fadeIn()
+        //         // $('.detailDiv').fadeIn()
+                
+        //     },
+        //     mouseleave: function () {
+        //         $(".detailDiv").removeClass('changeDetailDiv')
+        //         $(this).next().fadeOut()
+        //         // $('.detailDiv').fadeOut()
+                
+        //     }
+        // });
+
+        // $(".listDiv").on({
+        //     mouseenter: function () {
+        //         $(this).find("i").addClass('changeOfHeart');
+                
+        //     },
+        //     mouseleave: function () {
+        //         $(this).find("i").removeClass('changeOfHeart')
+            
+                
+        //     }
+        // });
+
+        
+        // $(".listDiv").closest("i").on("click", function(){
+        //     console.log("heart clicked ")
+        //     $(event.target).toggleClass("fas")
+        // })
+
+        // $(".divImage").on("click", function(){
+        //     console.log($(event.target).attr("alt"))
+        //     let favCharacter = $(event.target).attr("alt")
+        //     // alert("added this character to your favorites")
+        //     squanchysFavoriteSquanch.push(favCharacter)
+
+        // })
+
+        
+        // $('.detailDiv').hide()
+    };
+};
 
 
-// const request = fetch()
-
-//     request()
-//         .then(response => response.json())
-//         .then(data => console.log(data.results))
-//         .catch(error => console.log(`Fetch failed: ${error}`))
 
 
 
@@ -226,16 +191,5 @@ $(window).scroll(function(){
 
 
 
-
-
-// let loopOne = function(){
-//     for(let i = 0; i < data.results.length; i++){
-//             console.log(data.results[i])
-//             console.log(data.info.next)
-//             urlC137 = data.info.next
-//             ricksArray.push(data.results[i])
-//             console.log(`Pushed ${data.results[i].name} to the character array`)
-//         }
-// }
 
   
